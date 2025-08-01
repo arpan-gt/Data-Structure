27f49325017d71ef4c82216daa2fedb57dd0de4a
arpan-gt <arpan.main@outlook.com>
2025-08-01 19:38:15 +0530

    loops questions

--- week1/loop.cpp
+++ week1/loop.cpp
@@ -1,88 +1,118 @@
 #include <iostream>
 using namespace std;
+#include <math.h>
 int main()
 {
 
   // ------------------ n natural numbers-----------------------------------
-  int n;
-  cout << "enter value for natural numbers\n";
-  cin >> n;
-  for (int i = 1; i <= n; i++)
+  // int n;
+  // cout << "enter value for natural numbers\n";
+  // cin >> n;
+  // for (int i = 1; i <= n; i++)
+  // {
+  //   cout << i << "\n";
+  // }
+
+  // //---------------------sum of n natural numbers-------------------------------
+  // int sum = 0, n2;
+  // cout << "enter numbers of natural number\n";
+  // cin >> n2;
+  // for (int i = 1; i <= n2; i++)
+  // {
+  //   sum += i;
+  // }
+  // cout << "sum of first " << n2 << " natural numbers is " << sum << "\n";
+
+  // // ----------------------while loop----------------------------------------------
+
+  // int n3 = 5;
+  // while (n3 > 0)
+  // {
+  //   cout << n3 << "\n";
+  //   n3--;
+  // }
+
+  // //--------------------square pattern * using for loop------------------------
+  // int n4;
+  // cout << "enter number of rows or columns\n";
+  // cin >> n4;
+  // for (int i = 0; i < n4; i++)
+  // {
+  //   for (int j = 0; j < n4; j++)
+  //   {
+  //     cout << "*";
+  //   }
+  //   cout << "\n";
+  // }
+
+  // //-----------------print num from n to 1
+  // int n5;
+  // cout << "enter value of n\n";
+  // cin >> n5;
+  // for (int i = n5; i >= 1; i--)
+  // {
+  //   cout << i << "\n";
+  // }
+
+  // //-------------------sum of digits of a number in while loop------------------------------------
+  // int n6, temp, sumofDigits = 0;
+  // cout << "enter value to calculate sum of digits\n";
+  // cin >> n6;
+  // temp = n6;
+  // while (temp > 0)
+  // {
+  //   int lastDigit = temp % 10;
+  //   sumofDigits += lastDigit;
+  //   temp /= 10;
+  // }
+  // cout << "sum of digits of " << n6 << " is " << sumofDigits << "\n";
+
+  // //--------------------sum of odd digits of a number-----------------------------
+  // int n7, temp2, oddDigitSum = 0;
+  // cout << "enter a number to find sum of odd digits\n";
+  // cin >> n7;
+  // temp2 = n7;
+
+  // while (temp2 > 0)
+  // {
+  //   int lastDigit = temp2 % 10;
+  //   if (lastDigit % 2 != 0)
+  //   {
+  //     oddDigitSum += lastDigit;
+  //   }
+  //   temp2 /= 10;
+  // }
+
+  // cout << "sum of odd digits of number " << n7 << " is " << oddDigitSum << "\n";
+
+  //----------------prime number or not
+
+  int inputPrimeNumber;
+  cout << "enter number to check whether it is prime or not\n";
+  cin >> inputPrimeNumber;
+  bool isPrime = true;
+  
+  if (inputPrimeNumber <= 1)
   {
-    cout << i << "\n";
+    cout << inputPrimeNumber << " is not Prime Number\n";
+    return 0;
   }
-
-  //---------------------sum of n natural numbers-------------------------------
-  int sum = 0, n2;
-  cout << "enter numbers of natural number\n";
-  cin >> n2;
-  for (int i = 1; i <= n2; i++)
+  for (int i = 2; i <= sqrt(inputPrimeNumber); i++)
   {
-    sum += i;
-  }
-  cout << "sum of first " << n2 << " natural numbers is " << sum << "\n";
-
-  // ----------------------while loop----------------------------------------------
-
-  int n3 = 5;
-  while (n3 > 0)
-  {
-    cout << n3 << "\n";
-    n3--;
-  }
-
-  //--------------------square pattern * using for loop------------------------
-  int n4;
-  cout << "enter number of rows or columns\n";
-  cin >> n4;
-  for (int i = 0; i < n4; i++)
-  {
-    for (int j = 0; j < n4; j++)
+    if (inputPrimeNumber % i == 0)
     {
-      cout << "*";
+      isPrime = false;
+      break;
     }
-    cout << "\n";
   }
 
-  //-----------------print num from n to 1
-  int n5;
-  cout << "enter value of n\n";
-  cin >> n5;
-  for (int i = n5; i >= 1; i--)
+  if (isPrime)
   {
-    cout << i << "\n";
+    cout << inputPrimeNumber << " is Prime Number\n";
   }
-
-  //-------------------sum of digits of a number in while loop------------------------------------
-  int n6, temp, sumofDigits=0;
-  cout << "enter value to calculate sum of digits\n";
-  cin >> n6;
-  temp = n6;
-  while (temp > 0)
+  else
   {
-    int lastDigit = temp % 10;
-    sumofDigits += lastDigit;
-    temp /= 10;
+    cout << inputPrimeNumber << " is not Prime Number\n";
   }
-  cout << "sum of digits of " << n6 << " is " << sumofDigits << "\n";
-
-  //--------------------sum of odd digits of a number-----------------------------
-  int n7, temp2, oddDigitSum=0;
-  cout << "enter a number to find sum of odd digits\n";
-  cin >> n7;
-  temp2 = n7;
-
-  while (temp2 > 0)
-  {
-    int lastDigit = temp2 % 10;
-    if (lastDigit % 2 != 0)
-    {
-      oddDigitSum += lastDigit;
-    }
-    temp2 /= 10;
-  }
-
-  cout << "sum of odd digits of number " << n7 << " is " << oddDigitSum << "\n";
   return 0;
 }
-// 1024
